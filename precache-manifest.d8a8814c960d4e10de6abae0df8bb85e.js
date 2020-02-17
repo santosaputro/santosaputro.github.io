63ecf5beea82329b5168be44dba95a531ec92441
santosaputro1 <tri1400018009@webmail.uad.ac.id>
2020-02-17 16:58:31 +0700

    Updates

---
+++ precache-manifest.d8a8814c960d4e10de6abae0df8bb85e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f372fe71b1f4076c6f08123ecfef5cd0",
+    "url": "/santosaputro.github.io/index.html"
+  },
+  {
+    "revision": "eaac760f5424678eb045",
+    "url": "/santosaputro.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "da07152b1d5a9285d3d6",
+    "url": "/santosaputro.github.io/static/js/2.d26a0c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/santosaputro.github.io/static/js/2.d26a0c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaac760f5424678eb045",
+    "url": "/santosaputro.github.io/static/js/main.68823ade.chunk.js"
+  },
+  {
+    "revision": "38d3f81331257363acc1",
+    "url": "/santosaputro.github.io/static/js/runtime-main.af1cd4bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/santosaputro.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
